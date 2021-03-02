dca7c0682c3672536568e968040979ce4d34c56c
yaitbella <78006635+yaitbella@users.noreply.github.com>
2021-03-02 14:34:03 -0500

    Add files via upload
    added smoothing stuff; still needs some work

--- ID_PWM.ino
+++ ID_PWM.ino
@@ -2,6 +2,10 @@
  double tempValue = 0;
  double OUTPUTatZERO = .500;
  double TEMPCOFF = .010; 
+ double averageTempF = 0;
+ double averageTempC = 0; 
+ double totalTempF = 0;
+ double totalTempC = 0;
  void setup() {
   //bitwise operations that set the frequency of
   //either pin 3 or 11's PWM frequency to 31kHz
@@ -9,7 +13,9 @@
 
   //sets pin 3 to output mode
   pinMode(3, OUTPUT); 
+  pinMode(tempPin, INPUT); 
 
+  
   Serial.begin(9600);
 }
 
@@ -19,10 +25,30 @@ void loop() {
   //with a duty cycle of the second parameter 
   //divided by 255
   analogWrite(3, 170); 
-  tempValue = analogRead(tempPin); 
-  double voltOut = tempValue * 0.004887;
-  double actTempC =  (voltOut - OUTPUTatZERO) / TEMPCOFF;
-  double actTempF = actTempC * 9/5 + 32;  
-  Serial.println(actTempF);
-  delay(100);
+
+   int numRead = 10;
+
+   averageTempF = 0; 
+   averageTempC = 0; 
+   totalTempF = 0; 
+   totalTempC = 0; 
+   for (int i = 0; i <= numRead; i++) {
+
+     tempValue = analogRead(tempPin);
+
+     //converting analog readings to C and F temps
+     double voltOut = tempValue * 0.004887;                  //convert analog value to voltage
+     double actTempC =  (voltOut - OUTPUTatZERO) / TEMPCOFF; //convert voltage to temp in C based on equation in data sheet
+     double actTempF = actTempC * 9/5 + 32;                  //convert C temp to F temp
+  
+     totalTempF = totalTempF + actTempF; 
+     totalTempC = totalTempC + actTempC;
+      
+   }
+
+  averageTempF = totalTempF / numRead; 
+  averageTempC = totalTempC / numRead;  
+
+  Serial.println(averageTempF);
+  delay(500);  
 }
